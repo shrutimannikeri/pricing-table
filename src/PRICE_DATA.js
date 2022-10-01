2fc67bc1b84202315b1516043c6d506031030a03
shrutimannikeri <33412806+shrutimannikeri@users.noreply.github.com>
2022-10-01 11:34:45 +0530

    Create PRICE_DATA.js

---
+++ src/PRICE_DATA.js
@@ -0,0 +1,38 @@
+export const PRICE_DATA=[
+    {
+    name:"Free",
+    amount_month: "$0",
+    user: "single user",
+    storage:"5GB Storage",
+    Projects:"Unlimited Public Projects",
+    Access:"Community Access",
+    private_projects: false,
+    phone_suport: false,
+    free_subdomain: false,
+    report: false
+},
+{
+    name:"Plus",
+    amount_month: "$9",
+    user: "5 User",
+    storage:"50GB Storage",
+    Projects:"Unlimited Public Projects",
+    Access:"Community Access",
+    private_projects: true,
+    phone_suport: true,
+    free_subdomain: true,
+    report: false
+},
+{
+    name:"Pro",
+    amount_month: "$40",
+    user: "Unlimited User",
+    storage:"500GB Storage",
+    Projects:"Unlimited Public Projects",
+    Access:"Community Access",
+    private_projects: true,
+    phone_suport: true,
+    free_subdomain: true,
+    report: true
+}
+]
